baf3d972e60e52c306e5671282d9e20b361b7093
Konrad Kapp <k_kapp@yahoo.com>
2017-06-24 00:38:34 +0200

    Finish notify interface for design selection

--- Staxx/include/design_selection.h
+++ Staxx/include/design_selection.h
@@ -6,6 +6,12 @@
 #include "button.h"
 #include <vector>
 
+typedef enum
+{
+	CONFIRM_DELETE,
+	NO_SELECTION
+} notify_text_type;
+
 /*
  * Game state for the design selection. Each shape is a clickble_tile object with a texture showing each
  * shape. Selected shapes are then used in the next game.
@@ -27,6 +33,17 @@ private:
 	void cancel();
 	void save_selection();
 	void delete_first_click();
+	void delete_selected_shapes();
+	void cancel_delete();
+
+	void turn_all_text_off();
+	void turn_text_off(notify_text_type notify_text);
+
+	void show_noselect_text(int delay);
+
+	int count_selected();
+
+	static Uint32 sdl_timer_textoff_callback(Uint32 interval, void * args);
 
 private:
 	
@@ -39,10 +56,17 @@ private:
 	SDL_Event * pass_event;
 
 	button save_button, cancel_button, delete_button;
+	button confirm_button;
+	button not_confirm_button;
 
 	std::vector<SDL_Texture *> confirm_text_textures;
 
 	std::vector<std::pair<SDL_Rect, SDL_Texture *> > confirm_text_rects_textures;
+	std::vector<std::pair<SDL_Rect, SDL_Texture *> > noselect_text_rects_textures;
 
 	bool draw_confirm_delete_text = false;
+	bool draw_noselect_text = false;
+	bool show_confirm_delete_buttons = false;
+
+	SDL_TimerID noselect_notify_timer = 0;
 };
--- Staxx/src/design_selection.cpp
+++ Staxx/src/design_selection.cpp
@@ -45,7 +45,27 @@ design_selection::design_selection(int grid_x, int grid_y)
 		textures.at("red"),
 		textures.at("red"),
 		bind(&design_selection::delete_first_click, this),
-		"Delete", renderer)
+		"Delete", renderer),
+
+	confirm_button(delete_button.get_coords().first - 110,
+		delete_button.get_coords().second + 50 + 250,
+		100,
+		50,
+		textures.at("dark red"),
+		textures.at("red"),
+		textures.at("red"),
+		bind(&design_selection::delete_selected_shapes, this),
+		"Confirm", renderer),
+
+	not_confirm_button(delete_button.get_coords().first + delete_button.get_size().first + 10,
+		confirm_button.get_coords().second,
+		100,
+		50,
+		textures.at("dark red"),
+		textures.at("red"),
+		textures.at("red"),
+		bind(&design_selection::cancel_delete, this),
+		"Cancel", renderer)
 {
 	init_selection();
 
@@ -55,6 +75,11 @@ design_selection::design_selection(int grid_x, int grid_y)
 
 	confirm_text_rects_textures = get_wrapped_text_rects_textures("DELETE THE SELECTED SHAPES?", arcade_font,
 		renderer, delete_button.get_coords().first + delete_button.get_size().first / 2 - open_width / 3, 
+		delete_button.get_coords().second + delete_button.get_size().second + 35,
+		open_width * 3 / 4, 40, 2);
+
+	noselect_text_rects_textures = get_wrapped_text_rects_textures("NO SHAPES SELECTED", arcade_font,
+		renderer, delete_button.get_coords().first + delete_button.get_size().first / 2 - open_width / 3,
 		delete_button.get_coords().second + delete_button.get_size().second + 35,
 		open_width * 3 / 4, 40, 2);
 }
@@ -96,6 +121,12 @@ void design_selection::update()
 	delete_button.update();
 
 	scroller.update(pass_event);
+
+	if (show_confirm_delete_buttons)
+	{
+		confirm_button.update();
+		not_confirm_button.update();
+	}
 }
 
 void design_selection::draw()
@@ -108,9 +139,23 @@ void design_selection::draw()
 	delete_button.draw();
 	scroller.draw();
 
+	if (show_confirm_delete_buttons)
+	{
+		confirm_button.draw();
+		not_confirm_button.draw();
+	}
+
 	if (draw_confirm_delete_text)
 	{
 		for (pair<SDL_Rect, SDL_Texture *> &rect_texture : confirm_text_rects_textures)
+		{
+			SDL_RenderCopy(renderer, rect_texture.second, NULL, &rect_texture.first);
+		}
+	}
+
+	if (draw_noselect_text)
+	{
+		for (pair<SDL_Rect, SDL_Texture *> &rect_texture : noselect_text_rects_textures)
 		{
 			SDL_RenderCopy(renderer, rect_texture.second, NULL, &rect_texture.first);
 		}
@@ -126,6 +171,16 @@ void design_selection::cancel()
 
 void design_selection::save_selection()
 {
+	turn_all_text_off();
+	if (noselect_notify_timer)
+	{
+		/*
+		*  TODO: check possible threading issues here
+		*/
+		SDL_bool remove_result = SDL_RemoveTimer(noselect_notify_timer);
+		noselect_notify_timer = 0;
+	}
+
 	usable_shapes.clear();
 
 	for (int i = 0; i < selectors.size(); i++)
@@ -137,12 +192,115 @@ void design_selection::save_selection()
 		}
 	}
 
+	if (usable_shapes.size() == 0)
+	{
+		show_noselect_text(5000);
+		return;
+	}
+
 	quit = true;
+}
+
+Uint32 design_selection::sdl_timer_textoff_callback(Uint32 interval, void * args)
+{
+	void ** args_arr = (void **)args;
+	design_selection * this_ptr = (design_selection *)args_arr[0];
+	notify_text_type * notify_text = (notify_text_type *)args_arr[1];
+
+	this_ptr->turn_text_off(*notify_text);
+
+	delete args_arr[1];
+	free(args);
+
+	return 0;
+}
+
+void design_selection::turn_text_off(notify_text_type notify_text)
+{
+
+	switch (notify_text)
+	{
+		case (CONFIRM_DELETE):
+		{
+			draw_confirm_delete_text = false;
+		} break;
+		case (NO_SELECTION):
+		{
+			draw_noselect_text = false;
+		} break;
+	}
+}
+
+void design_selection::turn_all_text_off()
+{
+	for (int i = CONFIRM_DELETE; i != NO_SELECTION; i++)
+	{
+		turn_text_off((notify_text_type)i);
+	}
+	turn_text_off(NO_SELECTION);
+}
+
+void design_selection::show_noselect_text(int delay)
+{
+
+	draw_noselect_text = true;
+
+	notify_text_type * notify_text = new notify_text_type;
+	*notify_text = NO_SELECTION;
+
+	void ** arg_ptrs = (void **)malloc(sizeof(void *) * 2);
+	arg_ptrs[0] = (void *)this;
+	arg_ptrs[1] = (void *)notify_text;
+
+	noselect_notify_timer = SDL_AddTimer(delay, design_selection::sdl_timer_textoff_callback, (void *)arg_ptrs);
+}
+
+int design_selection::count_selected()
+{
+	int num_selected = 0;
+	for (int i = 0; i < selectors.size(); i++)
+	{
+		shared_ptr<clickable_tile> &tile_ptr = selectors.at(i);
+		if (tile_ptr->get_activation_level() == 2)
+		{
+			num_selected++;
+		}
+	}
+	return num_selected;
 }
 
 void design_selection::delete_first_click()
 {
+	turn_all_text_off();
+	if (noselect_notify_timer)
+	{
+		/*
+		*  TODO: check possible threading issues here
+		*/
+		SDL_bool remove_result = SDL_RemoveTimer(noselect_notify_timer);
+		noselect_notify_timer = 0;
+	}
+
+	if (count_selected() == 0)
+	{
+		show_noselect_text(5000);
+		return;
+	}
+
 	draw_confirm_delete_text = true;
+	show_confirm_delete_buttons = true;
+}
+
+void design_selection::delete_selected_shapes()
+{
+	cout << "Delete code must still be written" << endl;
+	show_confirm_delete_buttons = false;
+}
+
+void design_selection::cancel_delete()
+{
+	draw_confirm_delete_text = false;
+	show_confirm_delete_buttons = false;
 }
 
 void design_selection::mainloop()
@@ -150,7 +308,7 @@ void design_selection::mainloop()
 	quit = false;
 	while (!quit)
 	{
-
+		bool left_button_pressed = false;
 		if (SDL_PollEvent(&sdl_event))
 		{
 			pass_event = &sdl_event;
@@ -170,14 +328,40 @@ void design_selection::mainloop()
 					cout << "up key pressed in game state class" << endl;
 				}
 			}
+			else if (sdl_event.type == SDL_MOUSEBUTTONDOWN)
+			{
+				/*
+				* solution to remove dialog text when activity is detected.
+				* very hackish solution. Hope to have something more solid in the future
+				*/
+				if (sdl_event.button.button == SDL_BUTTON_LEFT)
+				{
+					turn_all_text_off();
+					if (noselect_notify_timer)
+					{
+						/*
+						*  TODO: check possible threading issues here
+						*/
+						SDL_bool remove_result = SDL_RemoveTimer(noselect_notify_timer);
+						noselect_notify_timer = 0;
+					}
+					left_button_pressed = true;
+				}
+			}
 		}
 		else
 		{
 			pass_event = nullptr;
 		}
 
-
 		update();
 		draw();
+
+		/*
+	    * part of solution to remove dialog text when activity is detected.
+		* very hackish solution. Hope to have something more solid in the future
+		*/
+		if (left_button_pressed)
+			show_confirm_delete_buttons = false;
 	}
 }
